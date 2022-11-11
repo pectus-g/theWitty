65164a6a09f36e6c307c7d1080a8a33f8ea3404c
ahk <80357049+pectus-g@users.noreply.github.com>
2022-11-11 14:41:58 +0300

    Projectile fireball

--- WITTY.v.00/Assets/Scripts/Combat/Projectile.cs
+++ WITTY.v.00/Assets/Scripts/Combat/Projectile.cs
@@ -8,14 +8,25 @@ namespace RPG.Combat
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed =1;
+
+    [SerializeField] bool isHoming = true;
+    [SerializeField] GameObject hitEffect =null;
+
     Health target = null;
     float damage =0;
 
+    void Start()
+    {
+        transform.LookAt(GetAimLocation());
+    }
     // Update is called once per frame
     void Update()
     {
         if(target ==null) return;
-        transform.LookAt(GetAimLocation());
+       if(isHoming && !target.IsDead())
+       {
+         transform.LookAt(GetAimLocation());
+       }
         transform.Translate(Vector3.forward*speed*Time.deltaTime);//move to the targer
 
     }
@@ -33,7 +44,14 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<Health>()!=target) return; // if collision object has health component take damage if not return null
+        if(target.IsDead()) return;
         target.TakeDamage(damage);
+
+        if(hitEffect!=null)
+        {
+            Instantiate(hitEffect,GetAimLocation(),transform.rotation);
+        }
+
         Destroy(gameObject);
     }
 }}
