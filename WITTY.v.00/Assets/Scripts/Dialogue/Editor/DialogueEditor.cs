6d1145af234621bad1b7d4491539cd88a6d9270f
ahk <80357049+pectus-g@users.noreply.github.com>
2022-12-22 17:07:00 +0300

    Dialogue Editor

--- WITTY.v.00/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ WITTY.v.00/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -8,22 +8,47 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
 {
+    Dialogue selectedDialogue=null;
+
     [MenuItem("Window/Dailogue Editor")]
     public static void ShowEditorWindow()
     {
        GetWindow(typeof(DialogueEditor),false,"Dialogue Editor");
     }
 
-    [OnOpenAssetAttribute(1)]
+    [OnOpenAsset(1)]
     public static bool OnOpenAsset(int instanceID,int line)
     {
-       Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;//casting for dialogur if it is not dialogu this will return null
+       Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;//casting for dialogur if it is not dialogue  this will return null
        if(dialogue!=null)
        {
       ShowEditorWindow();
         return true;
        }
         return false;
+    }
+    private void OnEnable() {
+        Selection.selectionChanged +=OnSelectionChanged;
+    }
+    private void OnSelectionChanged() {// name of function does not matter y
+      Dialogue newDialogue= Selection.activeObject as Dialogue;
+      if(newDialogue !=null)
+      {
+        selectedDialogue=newDialogue;
+        Repaint();
+      }
+    }
+    private void OnGUI()
+    {
+        if(selectedDialogue== null)
+        {
+        EditorGUILayout.LabelField("No Dialogue Selected");
+        }
+        else 
+        {
+        EditorGUILayout.LabelField(selectedDialogue.name);
+        }
+     
     }
 }
 
