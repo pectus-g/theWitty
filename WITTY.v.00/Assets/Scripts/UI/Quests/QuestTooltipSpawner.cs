97226c84327d3dba7649d75c2f3b75ed6e52b17a
ahk <80357049+pectus-g@users.noreply.github.com>
2023-01-02 20:29:03 +0300

    Quest added

---
+++ WITTY.v.00/Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevTV.Core.UI.Tooltips;
+
+namespace RPG.UI.Quests
+{
+    public class QuestTooltipSpawner : TooltipSpawner
+{
+   public override bool CanCreateTooltip()
+   {
+return true;
+   }
+   public override void UpdateTooltip(GameObject tooltip)
+   {
+
+   }
+
+}
+}
+
