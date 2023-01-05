847fd12b17751160801331e905fb828d04846d97
ahk <80357049+pectus-g@users.noreply.github.com>
2023-01-05 18:02:25 +0300

    Save for quests
    It has a bug not working

--- WITTY.v.00/Assets/Scripts/Quests/QuestList.cs
+++ WITTY.v.00/Assets/Scripts/Quests/QuestList.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using GameDevTV.Saving;
+
 
 namespace RPG.Quests
 {
-    public class QuestList : MonoBehaviour
+    public class QuestList : MonoBehaviour, ISaveable
 {
     List<QuestStatus> statuses= new List<QuestStatus>();
   
@@ -46,6 +48,27 @@ public void CompleteObjective(Quest quest, string objective)
                 }
             }
             return null;
+        }
+        public object CaptureState()
+        {
+            List<object> state = new List<object>();
+            foreach (QuestStatus status in statuses)
+            {
+                state.Add(status.CaptureState());
+            }
+            return state;
+        }
+
+        public void RestoreState(object state)
+        {
+            List<object> stateList = state as List<object>;
+            if (stateList == null) return;
+
+            statuses.Clear();
+            foreach (object objectState in stateList)
+            {
+                statuses.Add(new QuestStatus(objectState));
+            }
         }
 }
 
--- WITTY.v.00/Assets/Scripts/Quests/QuestStatus.cs
+++ WITTY.v.00/Assets/Scripts/Quests/QuestStatus.cs
@@ -10,11 +10,25 @@ namespace RPG.Quests
 {
    Quest quest;
    List<string> completedObjectives = new List<string>();
+    
+        [System.Serializable]
+        class QuestStatusRecord
+        {
+            public string questName;
+            public List<string> completedObjectives;
+        }
 
     public QuestStatus(Quest quest)
       {
             this.quest=quest;
         }
+        public QuestStatus(object objectState)
+        {
+            QuestStatusRecord state = objectState as QuestStatusRecord;
+            quest = Quest.GetByName(state.questName);
+            completedObjectives = state.completedObjectives;
+        }
+
    public Quest GetQuest()
    {
     return quest;
@@ -34,6 +48,13 @@ namespace RPG.Quests
             {
                 completedObjectives.Add(objective);
             }
+        }
+         public object CaptureState()
+        {
+            QuestStatusRecord state = new QuestStatusRecord();
+            state.questName = quest.name;
+            state.completedObjectives = completedObjectives;
+            return state;
         }
 }
 }
