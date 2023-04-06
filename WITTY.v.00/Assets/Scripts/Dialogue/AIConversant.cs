427ed01d360eb9622f8f920bd55213e7441fcc00
ahk <80357049+pectus-g@users.noreply.github.com>
2023-04-06 12:29:07 +0300

    Menu Design
    Menu Design Over

--- WITTY.v.00/Assets/Scripts/Dialogue/AIConversant.cs
+++ WITTY.v.00/Assets/Scripts/Dialogue/AIConversant.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Control;
 
-
+using RPG.Attributes;
 namespace RPG.Dialogue
 {
 public class AIConversant : MonoBehaviour, IRaycastable//when you hover over the npc we want a new cursor type
@@ -20,6 +20,7 @@ public class AIConversant : MonoBehaviour, IRaycastable//when you hover over the
    {
     return false;
    }
+    if (GetComponent<Health>().IsDead()) return false;
    if(Input.GetMouseButtonDown(0))
    {
     callingController.GetComponent<PlayerConversant>().StartDialogue(this,dialogue);
